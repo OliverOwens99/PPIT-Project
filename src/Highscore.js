c0d47c5b3ab1fbc3df1f583388b12fa60a8b61bb
olly <ollythemadbastard@gmail.com>
2024-04-24 23:04:38 +0100

    made sosme sytle changes

--- src/Highscore.js
+++ src/Highscore.js
@@ -1,41 +1,46 @@
+// Import necessary modules from React and CSS file
 import React, { useState, useEffect } from 'react';
 import './Highscore.css';
 
+// Define HighScores component
 function HighScores() {
+  // Initialize state for high scores with an empty array
   const [highScores, setHighScores] = useState([]);
 
+  // Use useEffect hook to fetch high scores data when component mounts
   useEffect(() => {
-    fetch('http://localhost:4000/highscores')
-      .then(response => response.json())
-      .then(data => setHighScores(data))
-      .catch(error => console.error('Error fetching high scores:', error));
-  }, []);
+    fetch('http://localhost:4000/highscores') // Fetch data from server
+      .then(response => response.json()) // Parse response as JSON
+      .then(data => setHighScores(data)) // Update highScores state with fetched data
+      .catch(error => console.error('Error fetching high scores:', error)); // Log any errors
+  }, []); // Empty dependency array means this effect runs once on mount
 
-  // This function generates a random color
+  // Function to generate a random color
   const getRandomColor = () => {
     const letters = '0123456789ABCDEF';
     let color = '#';
     for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+      color += letters[Math.floor(Math.random() * 16)]; // Generate a random hexadecimal color
     }
     return color;
   };
+
+  // Render the component
   return (
     <div>
-      <h1 className="highscores-heading">High Scores</h1>
-      <table className="highscores-table">
-        <thead>
+      <h1 className="highscores-heading">High Scores</h1> 
+      <table className="highscores-table"> 
+        <thead> 
           <tr>
             <th>Username</th>
             <th>Score</th>
           </tr>
         </thead>
-        <tbody>
-          {highScores.map((score, index) => (
+        <tbody> 
+          {highScores.map((score, index) => ( // Map over highScores array to create a row for each score
             <tr key={index}>
-              {/* Apply the random color to the username */}
-              <td style={{ color: getRandomColor() }}>{score.username}</td>
-              <td className="highscores-score">{score.score}</td>
+              <td style={{ color: getRandomColor() }}>{score.username}</td> 
+              <td className="highscores-score">{score.score}</td> 
             </tr>
           ))}
         </tbody>
@@ -44,4 +49,4 @@ function HighScores() {
   );
 }
 
-export default HighScores;
+export default HighScores; // Export HighScores component
